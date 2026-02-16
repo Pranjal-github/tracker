73533f87aef3cd4c69cab00fd6372dff3cf167d3
Pranjal <68186033+Pranjal-github@users.noreply.github.com>
2026-02-16 17:32:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,17 @@
 const CACHE = 'v1';
+
+// We use relative paths './' so GitHub Pages doesn't get confused
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(['/syll4.html','/syllabus.json','/manifest.json','/']))
+    caches.open(CACHE).then(cache => cache.addAll([
+      './',
+      './index.html',
+      './syllabus.json',
+      './manifest.json'
+    ]))
   );
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
